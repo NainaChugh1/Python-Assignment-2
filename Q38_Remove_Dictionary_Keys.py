8500419777474716356d8ddeef5218a47bdad8ff
NainaChugh1 <nchugh481@gmail.com>
2026-07-17 17:46:27 +0530

    Created Q38_Remove_Dictionary_Keys.py

---
+++ Q38_Remove_Dictionary_Keys.py
@@ -0,0 +1,16 @@
+# Q38 - Remove given keys from dictionary
+
+d = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New York"
+}
+
+keys_to_remove = ["name", "salary"]
+
+for key in keys_to_remove:
+    if key in d:
+        del d[key]
+
+print(d)
